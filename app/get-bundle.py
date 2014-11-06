5cf653aaf2d595e98c5aa67386cb529d2c79c94e
Callum Taylor <callum@3sidedcube.com>
2014-11-06 10:24:56 +0000

    Initial project setup

---
+++ app/get-bundle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import urllib2
+from urllib2 import HTTPError
+import sys
+import time
+import json
+import os
+import shutil
+import tarfile
+
+if len(sys.argv) <= 1:
+	print "Missing society identifier!"
+	exit(1)
+
+try:
+	url = "http://gdpc.cubeapis.com/v1.4/apps/" + str(sys.argv[1]) + "/bundle"
+	print "Downloading from " + url
+except KeyError:
+	print "Society " + str(sys.argv[1]) + " does not exist!"
+	exit(2)
+
+try:
+	bundle = urllib2.urlopen(url)
+
+	with open(str(sys.argv[1]) + '_bundle.tar.gz', 'wb') as archive:
+		archive.write(bundle.read())
+except HTTPError, e:
+	print "HttpError: " + str(e.code) + " " + url
+
+bundle = tarfile.open(str(sys.argv[1]) + '_bundle.tar.gz', 'r:gz')
+
+os.chdir("src/main/")
+if os.path.exists("assets") and os.path.isdir("assets"):
+	shutil.rmtree("assets")
+os.makedirs("assets")
+os.chdir("assets")
+
+bundle.extractall(".")
+os.chdir("../../..")
+os.remove(str(sys.argv[1]) + "_bundle.tar.gz")
---
+++ app/src/main/java/stormcorp/co/awdemo/BootActivity.java
@@ -0,0 +1,18 @@
+package stormcorp.co.awdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author Callum Taylor
+ * @project AppsWorld
+ */
+public class BootActivity extends Activity
+{
+	@Override protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
